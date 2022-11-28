6084eff232d38fbf2be116ec0c0fdcc7e6390990
John Rake <john@gorillabotlabs.com>
2022-11-28 13:22:48 -0600

    routes config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import "./App.css"
 import HomePage from './pages/HomePage';
+import ROUTES from './config/routes';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path={ROUTES.HOME} element={<HomePage />} />
       </Routes>
     </div>
   );
---
+++ src/config/routes.js
@@ -0,0 +1,6 @@
+const ROUTES = Object.freeze({
+    HOME: "/",
+    ONBOARDING: "/setup"
+})
+
+export default ROUTES
